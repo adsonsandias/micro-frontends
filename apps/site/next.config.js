eec9b659aae6bfbf0cf8c62df2a31fdd839bcce0
Adson Macbook Air <adsonbmx18@gmail.com>
2025-04-11 10:31:13 -0300

    refactor(site): simplify next.config.js by removing unused code
    Remove commented-out code and unnecessary headers configuration to improve maintainability and readability. Switch from ES module export to CommonJS for consistency with the project setup.

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,35 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-// const path = require('path')
-
 const nextConfig = {
-  // reactStrictMode: true,
-  // transpilePackages: ['@repo/ui'],
-  output: 'standalone',
-  // experimental: {
-  //   outputFileTracingRoot: path.join(__dirname, '../../'),
-  // },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // Set your origin
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
-    ]
-  },
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"],
+  output: 'standalone'
 }
 
-export default nextConfig
+module.exports = nextConfig
